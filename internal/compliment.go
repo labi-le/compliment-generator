831fdf0334e4bce7141c82292e5497bbfc750861
labile <1a6i1e@gmail.com>
2022-08-17 03:57:26 +0300

    refactor compliment

--- internal/compliment.go
+++ internal/compliment.go
@@ -2,6 +2,16 @@ package internal
 
 import (
 	"gopkg.in/telebot.v3"
+	"test/internal/daemon"
+	"test/pkg"
+	"time"
+)
+
+var (
+	menu = telebot.ReplyMarkup{}
+
+	btnStart = menu.Text("/start")
+	btnStop  = menu.Text("/stop")
 )
 
 type Compliments []*Compliment
@@ -12,19 +22,37 @@ type Compliment struct {
 	NeedStop  bool
 }
 
-func (cmd *Command) Compliment(c telebot.Context) error {
+func genGoal(c *telebot.Bot, r telebot.Recipient) daemon.Goal {
+	return func(t *daemon.Task) error {
+		menu.Reply(
+			menu.Row(btnStart),
+			menu.Row(btnStop),
+		)
 
-	Add(&Compliment{
-		UserName:  c.Sender(),
-		IsStarted: false,
-		NeedStop:  false,
+		compliment, _ := pkg.GetCompliment()
+		_, err := c.Send(r, compliment.Text, &menu)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+}
+
+func (cmd *Command) Compliment(c telebot.Context) error {
+	daemon.Add(&daemon.Task{
+		ID:          c.Sender().Recipient(),
+		Goal:        genGoal(c.Bot(), c.Sender()),
+		IsStarted:   false,
+		NeedStop:    false,
+		RepeatEvery: 1 * time.Second,
 	})
 
 	return nil
 }
 
 func (cmd *Command) StopCompliment(c telebot.Context) error {
-	Delete(c.Sender())
+	daemon.Delete(c.Sender().Recipient())
 
 	menu.RemoveKeyboard = true
 	return c.Reply("Хорошо", &menu)
